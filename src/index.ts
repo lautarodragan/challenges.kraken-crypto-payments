fb719bd4b2dfa5256b315e9ac28750fa67326e90
Lautaro Dragan <lautaro.dragan@gmail.com>
2020-02-18 03:32:10 -0300

    fix tsconfig: use commonjs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
-import { Collection, Binary } from 'mongodb'
+import { MongoClient } from 'mongodb'
 
 async function main() {
+  const mongoClient = await MongoClient.connect('mongodb://kraken:krakenPass@mongo:27017/kraken')
+  const dbConnection = await mongoClient.db()
+  const collection1 = dbConnection.collection('listsinceblock1')
+
+  await collection1.insertOne({ hihi: true })
+
+  await mongoClient.close()
+
   console.log('hello world')
 }
 
