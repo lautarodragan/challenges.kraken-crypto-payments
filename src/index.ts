9599d685538b23b1959c94ed41bbd7af16aca687
Lautaro Dragan <lautaro.dragan@gmail.com>
2020-02-18 05:23:40 -0300

    load transactions-1 into db

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
+import { readFile as readFileCb } from 'fs'
+import { promisify } from 'util'
+
 import { MongoClient } from 'mongodb'
 
+const readFile = promisify(readFileCb)
+
 async function main() {
-  const mongoClient = await MongoClient.connect('mongodb://kraken:krakenPass@mongo:27017/kraken')
+  const mongoClient = await MongoClient.connect('mongodb://localhost:27017/kraken', { useUnifiedTopology: true })
   const dbConnection = await mongoClient.db()
   const collection1 = dbConnection.collection('listsinceblock1')
 
-  await collection1.insertOne({ hihi: true })
+  await collection1.createIndex({ txid: 1 }, { unique: true })
+
+  const file = JSON.parse(await readFile('./challenge/transactions-1.json', 'utf8'))
+
+  try {
+    await collection1.insertMany(file.transactions)
+  } catch (error) {
+    if (!error.message.includes('E11000 duplicate key error'))
+      throw error
+  }
 
   await mongoClient.close()
 
