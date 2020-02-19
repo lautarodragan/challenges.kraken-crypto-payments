53881fd4054f9e678aeee05f3f8cf4e0a583ab78
Lautaro Dragan <lautaro.dragan@gmail.com>
2020-02-19 01:46:35 -0300

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,35 @@
 import { readFile as readFileCb } from 'fs'
 import { promisify } from 'util'
 
-import { MongoClient } from 'mongodb'
+import { MongoClient, Db } from 'mongodb'
 
 const readFile = promisify(readFileCb)
 
 async function main() {
   const mongoClient = await MongoClient.connect('mongodb://localhost:27017/kraken', { useUnifiedTopology: true })
   const dbConnection = await mongoClient.db()
-  const collection1 = dbConnection.collection('listsinceblock1')
-  const collection2 = dbConnection.collection('listsinceblock2')
 
-  await collection1.createIndex({ txid: 1 }, { unique: true })
-  await collection2.createIndex({ txid: 1 }, { unique: true })
+  await fileToCollection('./challenge/transactions-1.json', dbConnection, 'listsinceblock1')
 
-  const file1 = JSON.parse(await readFile('./challenge/transactions-1.json', 'utf8'))
+  await mongoClient.close()
 
-  console.log(`Adding ${file1.transactions.length} entries from transactions-1.json to DB...`)
+  console.log('Bye bye!')
+}
+
+async function fileToCollection(filePath: string, dbConnection: Db, collectionName: string) {
+  const collection = dbConnection.collection(collectionName)
+  await collection.createIndex({ txid: 1 }, { unique: true })
+
+  const file = JSON.parse(await readFile(filePath, 'utf8'))
+
+  console.log(`Adding ${file.transactions.length} entries from ${filePath} to DB.${collectionName}...`)
   try {
-    await collection1.insertMany(file1.transactions, { ordered: false })
+    await collection.insertMany(file.transactions, { ordered: false })
   } catch (error) {
     if (!error.message.includes('E11000 duplicate key error'))
       throw error
     console.log(`Ignored ${error.writeErrors.length} duplicate entries.`)
   }
-
-  await mongoClient.close()
-
-  console.log('hello world')
 }
 
 main().catch(console.error)
