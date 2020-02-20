180b2921a0d70cf8c5b2b528b34299ffd63f140c
Lautaro Dragan <lautaro.dragan@gmail.com>
2020-02-20 06:19:25 -0300

    more thoughts

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,24 @@ import { MongoClient, Db } from 'mongodb'
 
 const readFile = promisify(readFileCb)
 
+const users = {
+  'Wesley Crusher': 'mvd6qFeVkqH6MNAS2Y2cLifbdaX5XUkbZJ',
+  'Leonard McCoy': 'mmFFG4jqAtw9MoCC88hw5FNfreQWuEHADp',
+  'Jonathan Archer': 'mzzg8fvHXydKs8j9D2a8t7KpSXpGgAnk4n',
+  'Jadzia Dax': '2N1SP7r92ZZJvYKG2oNtzPwYnzw62up7mTo',
+  'Montgomery Scott': 'mutrAf4usv3HKNdpLwVD4ow2oLArL6Rez8',
+  'James T. Kirk': 'miTHhiX3iFhVnAEecLjybxvV5g8mKYTtnM',
+  'Spock': 'mvcyJMiAcSXKAEsQxbW9TYZ369rsMG6rVV',
+}
+
 async function main() {
   const mongoClient = await MongoClient.connect('mongodb://localhost:27017/kraken', { useUnifiedTopology: true })
   const dbConnection = await mongoClient.db()
 
   await fileToCollection('./challenge/transactions-1.json', dbConnection, 'listsinceblock1')
   await fileToCollection('./challenge/transactions-2.json', dbConnection, 'listsinceblock2')
+
+  await f(dbConnection, 'listsinceblock1')
 
   await mongoClient.close()
 
@@ -32,6 +44,19 @@ async function fileToCollection(filePath: string, dbConnection: Db, collectionNa
       return console.error('Unexpected error:', error)
     console.log(`Added ${error.result.nInserted} entries. Ignored ${error.result.result.writeErrors.length} duplicate entries.`)
   }
+}
+
+async function f(dbConnection: Db, collectionName: string) {
+  const collection = dbConnection.collection(collectionName)
+
+  const findTransactionsByUser = async ([username, address]: [string, string]) => {
+    console.log('Finding transactions for', username, address)
+    const x = await collection.find({ address, category: 'receive' }).toArray()
+    console.log(`Found ${x.length} transactions for ${username}`)
+
+  }
+
+  await Promise.all(Object.entries(users).map(findTransactionsByUser))
 }
 
 const isDuplicateKeyError = (error: any) => error.code === 11000
